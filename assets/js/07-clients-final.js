0a00ca6a20fafd1cc8d384f96a1fd5c60280b2ec
header911 <131144337+header911@users.noreply.github.com>
2026-07-26 01:07:26 +0300

    Add files via upload

--- assets/js/07-clients-final.js
+++ assets/js/07-clients-final.js
@@ -17,7 +17,7 @@
 */
 (function(){
   'use strict';
-  var VERSION='57.5.0-print-layout-repair';
+  var VERSION='57.6.0-print-layout-fix';
   var state={filter:'all',sort:'activity'};
 
   function $(id){return document.getElementById(id)}
@@ -210,8 +210,8 @@
    Loaded last on purpose. It wraps final write functions only, without changing calculations or data model. */
 (function(){
   'use strict';
-  var VERSION='57.5.0-print-layout-repair';
-  var SITE_VERSION='57_5printfix';
+  var VERSION='57.6.0-print-layout-fix';
+  var SITE_VERSION='57_6printlayout';
   var LOCAL_KEY='hayder_bags_app';
   var PENDING_KEY='hayder_pack_sync_pending_v37';
   var META_KEY='hayder_pack_sync_meta_v37';
@@ -313,8 +313,8 @@
 /* ===== V50 Backup Center Pro + Mobile Back Guard ===== */
 (function(){
   'use strict';
-  var VERSION='57.5.0-print-layout-repair';
-  var SITE_VERSION='57_5printfix';
+  var VERSION='57.6.0-print-layout-fix';
+  var SITE_VERSION='57_6printlayout';
   var booted=false;
   function $(id){return document.getElementById(id)}
   function q(s,r){return (r||document).querySelector(s)}
